cda251e8bad57e7d196cf1fdd06dc4e398dcdb40
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-07-06 13:52:06 +0200

    Refactor contacts handler

--- pages/api/contacts.ts
+++ pages/api/contacts.ts
@@ -1,4 +1,8 @@
-import { ApiResponse, connectToDb } from "@danielmat/api-utils"
+import {
+  ApiResponse,
+  connectToDb,
+  MethodNotAllowedError,
+} from "@danielmat/api-utils"
 import catchErrors from "@danielmat/api-utils/dist/catchErrors"
 import type { NextApiRequest, NextApiResponse } from "next"
 import ContactsDAO from "../../server/ContactsDAO"
@@ -8,25 +12,37 @@ import {
   removeUndefinedProperties,
 } from "../../utils/validation"
 
-async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ApiResponse<ContactsResponse>>
-) {
+interface ContactsHandler {
+  (
+    req: NextApiRequest,
+    res: NextApiResponse<ApiResponse<ContactsResponse>>
+  ): Promise<void>
+}
+
+const handler: ContactsHandler = async (req, res) => {
+  switch (req.method) {
+    case "GET":
+      await getHandler(req, res)
+      break
+    default:
+      throw new MethodNotAllowedError()
+  }
+}
+
+const getHandler: ContactsHandler = async (req, res) => {
   const db = await connectToDb()
   const contactsDAO = new ContactsDAO(db)
 
-  if (req.method == "GET") {
-    const { filter, sort, page } =
-      await getContactsQueryParametersValidation.validate(req.query)
+  const { filter, sort, page } =
+    await getContactsQueryParametersValidation.validate(req.query)
 
-    const result = await contactsDAO.getContacts(
-      removeUndefinedProperties(filter),
-      sort,
-      page
-    )
+  const result = await contactsDAO.getContacts(
+    removeUndefinedProperties(filter),
+    sort,
+    page
+  )
 
-    res.status(200).json({ status: "success", ...result })
-  }
+  res.status(200).json({ status: "success", ...result })
 }
 
 export default catchErrors(handler)
