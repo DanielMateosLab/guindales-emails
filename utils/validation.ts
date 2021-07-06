0be7c18e001981bb2f70abae51c04b38c24df7eb
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-07-06 13:17:44 +0200

    Refactor sort validation code to be simpler

--- utils/validation.ts
+++ utils/validation.ts
@@ -1,33 +1,9 @@
 import * as yup from "yup"
-import { Contact } from "./types"
-
-const allowedSortFields: Array<keyof Contact> = ["_id", "name"]
-const sortFieldErrorText = `Invalid sort property.\
-  Allowed sort fields: ${allowedSortFields.join(", ")}.\
-  Sort value must be 1 for ascending order or -1 for descending order.\
-  Example: { _id: 1, name: -1}.`
 
 export const getContactsQueryParametersValidation = yup.object().shape({
-  sort: yup.object().test("sortTest", sortFieldErrorText, (sortParams) => {
-    if (sortParams == null) {
-      return true
-    }
-
-    if (typeof sortParams == "object") {
-      let isValid = true
-
-      Object.keys(sortParams).forEach((key) => {
-        if (keyIsValid(key) && sortValueIsValid(sortParams[key])) {
-          return
-        } else {
-          isValid = false
-        }
-      })
-
-      return isValid
-    }
-
-    return false
+  sort: yup.object().shape({
+    _id: sortValueIsOneOrMinusOne(),
+    name: sortValueIsOneOrMinusOne(),
   }),
   filter: yup.object().shape({
     name: yup.string().max(55),
@@ -37,11 +13,17 @@ export const getContactsQueryParametersValidation = yup.object().shape({
   page: yup.number().positive().integer(),
 })
 
-function keyIsValid(key: string) {
-  return allowedSortFields.some((value) => value == key)
+function sortValueIsOneOrMinusOne() {
+  return yup
+    .number()
+    .test(
+      "sortTest",
+      "Sort value must be 1 for ascending order or -1 for descending order",
+      (value) => (valueIsDefined(value) ? value == 1 || value == -1 : true)
+    )
 }
-function sortValueIsValid(value: any) {
-  return value == 1 || value == -1
+function valueIsDefined(value: any) {
+  return value !== undefined
 }
 
 interface Object {
