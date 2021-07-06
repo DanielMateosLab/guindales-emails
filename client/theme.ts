b060c75b811f23c56bafe57a03183267abcc2337
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-07-06 19:30:25 +0200

    Update theme palette

--- client/theme.ts
+++ client/theme.ts
@@ -1,13 +1,19 @@
-import { red } from "@material-ui/core/colors"
+import { blue, orange, red } from "@material-ui/core/colors"
 import { createMuiTheme } from "@material-ui/core/styles"
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#556cd6",
+      main: blue[400],
+      light: "#9be7ff",
+      dark: "#2286c3",
+      contrastText: "#000000",
     },
     secondary: {
-      main: "#19857b",
+      main: orange[400],
+      light: "#ffe97d",
+      dark: "#c88719",
+      contrastText: "#000000",
     },
     error: {
       main: red.A400,
