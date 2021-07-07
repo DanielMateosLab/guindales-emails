0332ba0569bdd24e29de08d84ea253c0e9a23ee2
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-07-07 08:20:18 +0200

    Add secondary color

--- client/theme.ts
+++ client/theme.ts
@@ -1,6 +1,8 @@
 import { red } from "@material-ui/core/colors"
 import { createMuiTheme } from "@material-ui/core/styles"
 
+// Color tool: https://material.io/resources/color/#!/?view.left=0&view.right=1&primary.color=65FC86
+
 const theme = createMuiTheme({
   palette: {
     primary: {
@@ -8,6 +10,12 @@ const theme = createMuiTheme({
       light: "#9effb7",
       dark: "#1bc857",
       contrastText: "#000000",
+    },
+    secondary: {
+      main: "#327d42",
+      light: "#62ad6e",
+      dark: "#005019",
+      contrastText: "#ffffff",
     },
     error: {
       main: red.A400,
