1cb590ae6b1ef9a44332a8743f2ebeefd0024146
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-09-02 16:48:13 +0200

    Add form fields

--- client/features/contacts/AddContactDialog.tsx
+++ client/features/contacts/AddContactDialog.tsx
@@ -4,11 +4,15 @@ import {
   IconButton,
   Toolbar,
   Typography,
+  useMediaQuery,
+  useTheme,
 } from "@material-ui/core"
 import Dialog from "@material-ui/core/Dialog"
 import Slide from "@material-ui/core/Slide"
 import { TransitionProps } from "@material-ui/core/transitions"
 import CloseIcon from "@material-ui/icons/Close"
+import TextField from "client/common/TextField"
+import { Form, Formik, FormikHelpers } from "formik"
 import { forwardRef, useState } from "react"
 import AddContactButton from "./AddContactButton"
 
@@ -17,11 +21,23 @@ const SlideTransition = forwardRef<
   TransitionProps & { children?: React.ReactElement }
 >((props, ref) => <Slide direction="up" ref={ref} {...props} />)
 
+const formInitialValues = { name: "", email: "", phone: "" }
+
 const AddContactDialog: React.FC = ({}) => {
   const [open, setOpen] = useState(false)
+  const theme = useTheme()
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"))
 
   function handleClose() {
     setOpen(false)
+  }
+
+  const handleSubmit = (
+    values: typeof formInitialValues,
+    { setSubmitting }: FormikHelpers<typeof formInitialValues>
+  ) => {
+    console.log(values)
+    setSubmitting(false)
   }
 
   return (
@@ -29,31 +45,80 @@ const AddContactDialog: React.FC = ({}) => {
       <AddContactButton onClick={() => setOpen(true)} />
 
       <Dialog
-        fullScreen
         open={open}
+        fullScreen={isSmallScreen}
         onClose={handleClose}
         TransitionComponent={SlideTransition}
       >
-        <AppBar component="section">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              onClick={handleClose}
-              aria-label="cerrar"
-            >
-              <CloseIcon />
-            </IconButton>
-            <Typography variant="h6" component="h1" className="app-bar-title">
-              Nuevo Contacto
-            </Typography>
-            <Button autoFocus color="inherit" onClick={handleClose}>
-              Guardar
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <main className="app-container"></main>
+        <Formik initialValues={formInitialValues} onSubmit={handleSubmit}>
+          {(formik) => (
+            <Form>
+              <AppBar position="relative" component="section">
+                <Toolbar>
+                  <IconButton
+                    edge="start"
+                    color="inherit"
+                    onClick={handleClose}
+                    aria-label="cerrar"
+                  >
+                    <CloseIcon />
+                  </IconButton>
+
+                  <Typography
+                    variant="h6"
+                    component="h1"
+                    className="app-bar-title add-contact-title"
+                  >
+                    Nuevo Contacto
+                  </Typography>
+
+                  <Button
+                    autoFocus
+                    color="inherit"
+                    type="submit"
+                    disabled={formik.isSubmitting}
+                  >
+                    Guardar
+                  </Button>
+                </Toolbar>
+              </AppBar>
+
+              <main className="app-container add-contact-form">
+                <TextField label="Nombre" name="name" placeholder="Jhon Doe" />
+                <TextField
+                  label="Email"
+                  name="email"
+                  placeholder="jhon_doe@example.com"
+                />
+                <TextField
+                  label="Teléfono"
+                  name="phone"
+                  placeholder="34 685 546 387"
+                />
+              </main>
+            </Form>
+          )}
+        </Formik>
       </Dialog>
+      <style global jsx>{`
+        .add-contact-title {
+          margin-inline: 1rem;
+        }
+
+        .add-contact-form {
+          display: flex;
+          flex-direction: column;
+          padding: 1rem;
+          gap: 1rem;
+          max-width: 600px;
+          margin: auto;
+        }
+
+        .add-contact-form label {
+          color: ${theme.palette.secondary.dark};
+          margin-bottom: 0.25rem;
+        }
+      `}</style>
     </>
   )
 }
