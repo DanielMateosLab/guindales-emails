d90be245319ba39cfff26af373544f0f8a7cc11c
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-10-04 18:21:09 +0200

    Add addContact method to ContactsDAO

--- server/ContactsDAO.ts
+++ server/ContactsDAO.ts
@@ -1,7 +1,7 @@
 import CollectionDAO from "@danielmat/api-utils/dist/CollectionDAO"
 import { Db } from "mongodb"
 import { pageSize } from "utils/config"
-import { Contact, ContactsDAOSortQuery } from "utils/types"
+import { Contact, ContactsDAOSortQuery, WithoutId } from "utils/types"
 
 export default class ContactsDAO extends CollectionDAO<Contact> {
   constructor(db: Db) {
@@ -23,5 +23,13 @@ export default class ContactsDAO extends CollectionDAO<Contact> {
     const count = await cursor.count()
 
     return { contacts, count }
+  }
+
+  /** Adds a contact and returns the generated _id */
+  async addContact(contact: WithoutId<Contact>): Promise<string> {
+    // Have to use "as any" in insertOne params to avoid the required _id field"
+    const result = await this.collection.insertOne(contact as any)
+
+    return result.insertedId
   }
 }
