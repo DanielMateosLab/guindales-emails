cceb04060d0924c38e640d611de950b4dc591f0e
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-10-04 19:10:10 +0200

    Add postHandler, update types and create setUpContactsDAO

--- pages/api/contacts.ts
+++ pages/api/contacts.ts
@@ -1,30 +1,28 @@
 import { connectToDb, MethodNotAllowedError } from "@danielmat/api-utils"
 import catchErrors from "@danielmat/api-utils/dist/catchErrors"
-import type { NextApiRequest, NextApiResponse } from "next"
+import type { NextApiHandler } from "next"
 import ContactsDAO from "server/ContactsDAO"
-import { SuccessContactsResponse } from "utils/types"
+import { Contact, SuccessContactsResponse } from "utils/types"
 import { getQueryValidation } from "utils/validation"
 
-interface ContactsHandler {
-  (
-    req: NextApiRequest,
-    res: NextApiResponse<SuccessContactsResponse>
-  ): Promise<void>
-}
-
-const handler: ContactsHandler = async (req, res) => {
+const handler: NextApiHandler = async (req, res) => {
   switch (req.method) {
     case "GET":
       await getHandler(req, res)
+      break
+    case "POST":
+      await postHandler(req, res)
       break
     default:
       throw new MethodNotAllowedError()
   }
 }
 
-const getHandler: ContactsHandler = async (req, res) => {
-  const db = await connectToDb()
-  const contactsDAO = new ContactsDAO(db)
+const getHandler: NextApiHandler<SuccessContactsResponse> = async (
+  req,
+  res
+) => {
+  const contactsDAO = await setUpContactsDAO()
 
   const { filter, sort, page } = await getQueryValidation.validate(req.query)
 
@@ -35,6 +33,17 @@ const getHandler: ContactsHandler = async (req, res) => {
   )
 
   res.status(200).json({ ...result })
+}
+
+const postHandler: NextApiHandler<Contact> = async (req, res) => {
+  const contactsDAO = await setUpContactsDAO()
+
+  // TODO: finish postHandler and keep going with contactsApiSlice
+}
+
+async function setUpContactsDAO() {
+  const db = await connectToDb()
+  return new ContactsDAO(db)
 }
 
 export default catchErrors(handler)
