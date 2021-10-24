c9fad7f9685dc0731c087c8326b9f15b27c69db8
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-10-24 10:57:21 +0200

    Add see-all button

--- client/features/contacts/FoundResultsText.tsx
+++ client/features/contacts/FoundResultsText.tsx
@@ -1,4 +1,5 @@
-import { Typography } from "@material-ui/core"
+import { Button, Typography } from "@material-ui/core"
+import theme from "client/app/theme"
 import { useEffect, useState } from "react"
 import { useContactResultsSelector } from "./contactResultsSlice"
 
@@ -15,7 +16,7 @@ const FoundResultsText: React.FC<{
     } else if (contacts.length == 0) {
       setText("No se han encontrado contactos.")
     } else {
-      setText(`Mostrando ${contacts.length} de ${count} emails encontrados`)
+      setText(`Mostrando ${contacts.length} de ${count} resultados. `)
     }
   }, [contacts.length, count, isLoading])
 
@@ -26,7 +27,20 @@ const FoundResultsText: React.FC<{
         style={{ color: isLoading ? "transparent" : "inherit" }}
       >
         {text}
+        {!isLoading && (
+          <Button size="small" className="see-all-button">
+            Ver todos
+          </Button>
+        )}
       </Typography>
+
+      <style global jsx>
+        {`
+          .see-all-button {
+            color: ${theme.palette.secondary.dark};
+          }
+        `}
+      </style>
     </article>
   )
 }
