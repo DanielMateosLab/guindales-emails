d5f51006cd10fa4ba3225ef770c63c1ba1672509
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-10-26 14:40:00 +0200

    Use CheckingSession in dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,16 @@
+import CheckingSession from "client/common/CheckingSession"
 import AddContactButton from "client/features/contacts/AddContactButton"
 import ContactListContainer from "client/features/contacts/ContactListContainer"
 import CreateOrUpdateContactDialog from "client/features/contacts/CreateOrUpdateContactDialog"
+import { useSession } from "next-auth/react"
 
 export default function Dashboard() {
   // TODO: redirect to index page if there is no session
   // OR return the checking credentials component as body and redirect only if it fails
+  const { status } = useSession({ required: false })
+
+  if (status == "loading") return <CheckingSession />
+
   return (
     <div>
       <ContactListContainer />
