3cf54a59f7e3ee435a8199ccfaf1fdb0f012fdae
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-10-26 11:05:42 +0200

    Move Home component to Dashboard

---
+++ pages/dashboard.tsx
@@ -0,0 +1,17 @@
+import Header from "client/common/Header"
+import AddContactButton from "client/features/contacts/AddContactButton"
+import ContactListContainer from "client/features/contacts/ContactListContainer"
+import CreateOrUpdateContactDialog from "client/features/contacts/CreateOrUpdateContactDialog"
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Header />
+
+      <ContactListContainer />
+      <CreateOrUpdateContactDialog>
+        {(openDialog) => <AddContactButton onClick={openDialog} />}
+      </CreateOrUpdateContactDialog>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,9 @@
 import Header from "client/common/Header"
-import AddContactButton from "client/features/contacts/AddContactButton"
-import ContactListContainer from "client/features/contacts/ContactListContainer"
-import CreateOrUpdateContactDialog from "client/features/contacts/CreateOrUpdateContactDialog"
 
 export default function Home() {
   return (
     <div>
       <Header />
-
-      <ContactListContainer />
-      <CreateOrUpdateContactDialog>
-        {(openDialog) => <AddContactButton onClick={openDialog} />}
-      </CreateOrUpdateContactDialog>
     </div>
   )
 }
