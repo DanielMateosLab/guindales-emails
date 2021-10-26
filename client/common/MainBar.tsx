799999791a85042864e5709d618b0e4381af039d
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-10-26 11:25:20 +0200

    Display SearchBar and its realted buttons to auth users only

--- client/common/MainBar.tsx
+++ client/common/MainBar.tsx
@@ -1,54 +1,61 @@
 import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core"
 import { Search, Sort } from "@material-ui/icons"
 import { MenuState, SwitchMenu } from "utils/types"
+import useIsAuthenticated from "./useIsAuthenticated"
 
 interface Props {
   menuState: MenuState
   switchMenu: SwitchMenu
 }
-const MainBar: React.FC<Props> = ({ menuState, switchMenu }) => (
-  <AppBar position="relative" component="section">
-    <Toolbar>
-      <Typography variant="h6" component="h1" className="app-bar-title">
-        Emails
-      </Typography>
-      <section className="menu-buttons">
-        <IconButton
-          className={menuState.searchMenu ? "active" : ""}
-          onClick={() => switchMenu("searchMenu")}
-          aria-label="Mostrar/ocultar menú de búsqueda"
-        >
-          <Search />
-        </IconButton>
-        <IconButton
-          className={menuState.sortMenu ? "active" : ""}
-          onClick={() => switchMenu("sortMenu")}
-          aria-label="Mostrar/ocultar menú de orden de resultados"
-        >
-          <Sort />
-        </IconButton>
-      </section>
-    </Toolbar>
+const MainBar: React.FC<Props> = ({ menuState, switchMenu }) => {
+  const isAuthenticated = useIsAuthenticated()
 
-    <style global jsx>
-      {`
-        .menu-buttons > button {
-          margin-right: 0.5rem;
-          padding: 8px;
-        }
-        .menu-buttons > button:last-child {
-          margin-right: 0;
-        }
+  return (
+    <AppBar position="relative" component="section">
+      <Toolbar>
+        <Typography variant="h6" component="h1" className="app-bar-title">
+          Emails
+        </Typography>
+        {isAuthenticated && (
+          <section className="menu-buttons">
+            <IconButton
+              className={menuState.searchMenu ? "active" : ""}
+              onClick={() => switchMenu("searchMenu")}
+              aria-label="Mostrar/ocultar menú de búsqueda"
+            >
+              <Search />
+            </IconButton>
+            <IconButton
+              className={menuState.sortMenu ? "active" : ""}
+              onClick={() => switchMenu("sortMenu")}
+              aria-label="Mostrar/ocultar menú de orden de resultados"
+            >
+              <Sort />
+            </IconButton>
+          </section>
+        )}
+      </Toolbar>
 
-        .app-bar-title {
-          flex-grow: 1;
-        }
-        .active {
-          background-color: rgba(0, 0, 0, 0.15) !important;
-        }
-      `}
-    </style>
-  </AppBar>
-)
+      <style global jsx>
+        {`
+          .menu-buttons > button {
+            margin-right: 0.5rem;
+            padding: 8px;
+          }
+          .menu-buttons > button:last-child {
+            margin-right: 0;
+          }
+
+          .app-bar-title {
+            flex-grow: 1;
+          }
+          .active {
+            background-color: rgba(0, 0, 0, 0.15) !important;
+          }
+        `}
+      </style>
+    </AppBar>
+  )
+}
 
 export default MainBar
