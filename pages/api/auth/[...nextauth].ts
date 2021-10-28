9f8653ea6d01c45b1f9c2850940c5177940c7ce5
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-10-28 11:55:41 +0200

    Set up db adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,16 +1,25 @@
+import { connectToDb } from "@danielmat/api-utils"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import { NextApiHandler } from "next"
 import NextAuth from "next-auth"
 import EmailProvider from "next-auth/providers/email"
 import GitHubProvider from "next-auth/providers/github"
 
-export default NextAuth({
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+const auth: NextApiHandler = async (req, res) =>
+  NextAuth(req, res, {
+    adapter: MongoDBAdapter({
+      db: await connectToDb(),
     }),
-    EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-    }),
-  ],
-})
+    providers: [
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+      }),
+      EmailProvider({
+        server: process.env.EMAIL_SERVER,
+        from: process.env.EMAIL_FROM,
+      }),
+    ],
+  })
+
+export default auth
