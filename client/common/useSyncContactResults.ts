2cada683e4b82c688013592672a1c3eee7617854
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-10-29 13:32:56 +0200

    Add aclarative comment

--- client/common/useSyncContactResults.ts
+++ client/common/useSyncContactResults.ts
@@ -3,6 +3,10 @@ import { useEffect } from "react"
 import { useAppDispatch } from "./reduxHooks"
 import useSearchBarContactsQuery from "./useSearchBarContactsQuery"
 
+/**
+ * Takes the results from the getContactsQuery, called with the search bar params, and updates
+ * the contacts results data (infinite scrolling feature)
+ */
 const useSyncContactResults = () => {
   const { data } = useSearchBarContactsQuery()
 
