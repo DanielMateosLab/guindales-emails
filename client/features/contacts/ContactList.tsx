cc7bdd58f06b55da424c89cfbfe6b52c562879dd
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-10-31 21:58:17 +0100

    Update contacts type

--- client/features/contacts/ContactList.tsx
+++ client/features/contacts/ContactList.tsx
@@ -1,7 +1,7 @@
-import { Contact } from "utils/types"
+import { HttpContact } from "utils/types"
 import ContactItem from "./ContactItem"
 
-const ContactList: React.FC<{ contacts: Contact[] }> = ({ contacts }) => (
+const ContactList: React.FC<{ contacts: HttpContact[] }> = ({ contacts }) => (
   <section aria-label="lista de emails">
     {contacts.map((contact) => (
       <ContactItem contact={contact} key={contact._id} />
